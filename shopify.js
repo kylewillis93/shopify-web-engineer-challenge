48a91ebdf52387d99acfc829db41ed17a3a9f479
Kyle Willis <kyle.c.willis93@gmail.com>
2019-01-11 18:05:58 -0800

    added loading spinner

--- shopify.js
+++ shopify.js
@@ -5,41 +5,40 @@ function getMatchingData(data, queryString) {
   let matches = [];
   for (var key in data) {
     if (data.hasOwnProperty(key)) {
-           data[key].uuid = key.toString();
-          if (data[key].title.toLowerCase().indexOf(queryString) !== -1) {
-            matches = matches.concat(data[key]);
-          } else if (data[key].keywords.toLowerCase().indexOf(queryString) !== -1) {
-            matches = matches.concat(data[key]);
-          }
+      data[key].uuid = key.toString();
+      if (data[key].title.toLowerCase().indexOf(queryString) !== -1) {
+        matches = matches.concat(data[key]);
+      } else if (data[key].keywords.toLowerCase().indexOf(queryString) !== -1) {
+        matches = matches.concat(data[key]);
       }
     }
-    return matches;
+  }
+  return matches;
 }
-//TODO
-//remember favorites across querries? // use UUIDs for items not key
-//host on ec2 instance
 
-function addToFavorites(uuid){
+//TODO loading spinner
+
+function addToFavorites(uuid) {
   if (favorites.indexOf(uuid.toString()) === -1) {
-  if (favorites.length === 0) {
-    let div = document.createElement('div');
-    let favBox = '<div class="favorites" id="fav-box">\
+    if (favorites.length === 0) {
+      let div = document.createElement('div');
+      let favBox = '<div class="favorites" id="fav-box">\
                   <h1 class="fav-title">Favourites</h1>\
                   <div class="results-container" id="favorites">\
                    </div>\
                   </div>';
-    div.innerHTML = div.innerHTML.concat(favBox);
-    document.getElementById('fav-footer').appendChild(div);
-  }
-  newFav = document.getElementById(uuid).cloneNode(true);
-  newFav.setAttribute("id", "fav"+uuid);
-  favorites = favorites.concat(uuid.toString());
-  document.getElementById('favorites').append(newFav);
+      div.innerHTML = div.innerHTML.concat(favBox);
+      document.getElementById('fav-footer').appendChild(div);
+    }
+    newFav = document.getElementById(uuid).cloneNode(true);
+    newFav.setAttribute("id", "fav" + uuid);
+    favorites = favorites.concat(uuid.toString());
+    document.getElementById('favorites').append(newFav);
   }
 }
 
-function removeFromFavorites(uuid){
-  let ele = document.getElementById("fav"+uuid);
+function removeFromFavorites(uuid) {
+  let ele = document.getElementById("fav" + uuid);
   ele.parentNode.removeChild(ele);
   let index = favorites.indexOf(uuid.toString());
   if (index > -1) {
@@ -52,21 +51,20 @@ function removeFromFavorites(uuid){
 }
 
 function changeImage(uuid) {
-  let srcString = document.getElementById("star"+uuid+"").src;
+  let srcString = document.getElementById("star" + uuid + "").src;
   srcString = srcString.substr(srcString.length - 12);
   if (srcString === "greystar.png") {
-      document.getElementById("star"+uuid+"").src = "./greenstar.png";
-      addToFavorites(uuid);
+    document.getElementById("star" + uuid + "").src = "./greenstar.png";
+    addToFavorites(uuid);
   }
   else {
-    document.getElementById("star"+uuid+"").src = "./greystar.png";  
+    document.getElementById("star" + uuid + "").src = "./greystar.png";
     removeFromFavorites(uuid);
   }
 }
 
-function appendSearchResults(matches){
+function appendSearchResults(matches) {
   var div = document.createElement('div');
-  div.className = 'row';
   //remove previous search results if they exist
   let testElement = document.getElementById('dyno-results');
   if (testElement !== null) {
@@ -80,14 +78,14 @@ function appendSearchResults(matches){
     } else {
       starImg = "./greenstar.png";
     }
-    let rowEle = "<div class='result-object' id="+matches[key].uuid+">";
+    let rowEle = "<div class='result-object' id=" + matches[key].uuid + ">";
     rowEle = rowEle.concat('<div class="result-ele0">\
-                            <img class="star-icon" id= "star'+matches[key].uuid+'"\
-                             onclick="changeImage('+matches[key].uuid+')"\
-                             src='+starImg+' />\
+                            <img class="star-icon" id= "star' + matches[key].uuid + '"\
+                             onclick="changeImage(' + matches[key].uuid + ')"\
+                             src=' + starImg + ' />\
                             </div>');
     rowEle = rowEle.concat('<div class="result-ele1">');
-    rowEle = rowEle.concat('<text>'+matches[key].title+'</text>');
+    rowEle = rowEle.concat('<text>' + matches[key].title + '</text>');
     rowEle = rowEle.concat('</div>');
     rowEle = rowEle.concat('<div class="result-ele2">');
     rowEle = rowEle.concat($("<p/>").html(matches[key].body).text());
@@ -98,20 +96,29 @@ function appendSearchResults(matches){
   document.getElementById('search-results').appendChild(div);
 }
 
-function searchTorontoWaste (){
-    let queryString = document.getElementById("query-input").value;
-    let matches = [];
-    if (queryString !== ""){
-      queryString = queryString.toLowerCase();
+function searchTorontoWaste() {
+  let queryString = document.getElementById("query-input").value;
+  let matches = [];
+  if (queryString !== "") {
+    queryString = queryString.toLowerCase();
     if (cachedData.length === 0) {
-    $.getJSON('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000', function(data) {
-    cachedData = data;
-     matches = getMatchingData(cachedData, queryString);
-     appendSearchResults(matches);
-    });
+      //start loading animation
+      let div = document.createElement('div');
+      div.innerHTML = div.innerHTML.concat('<div class="loading" id="loading">\
+                            <img class="loading-spinner" src="loading-spinner.gif"></img>\
+                            </div>');
+      document.getElementById('search-results').appendChild(div);
+      console.log(document.getElementById('search-results'));
+      $.getJSON('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000', function (data) {
+        cachedData = data;
+        matches = getMatchingData(cachedData, queryString);
+        //end loading animation
+        div.parentNode.removeChild(div);
+        appendSearchResults(matches);
+      });
     } else {
       matches = getMatchingData(cachedData, queryString);
       appendSearchResults(matches);
     }
-    }
+  }
 };
